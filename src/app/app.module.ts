b4b9b4eba37dc88f51a039d7f5dd83a13773746f
o.muslimov <oybekmuslimov15@gmail.com>
2022-05-19 15:50:55 +0300

    Добавил контроллер

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
-import {AuthModule} from './auth/auth.module';
-import {UserModule} from './user/user.module';
-import {PromocodeModule} from './promocode/promocode.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PromocodeModule } from './promocode/promocode.module';
 
-import {CommonModule} from '../common/common.module';
+import { CommonModule } from '../common/common.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [
-    CommonModule,
-    PromocodeModule,
-    UserModule,
-    AuthModule
-  ],
+  imports: [CommonModule, PromocodeModule, UserModule, AuthModule],
+  controllers: [AppController],
 })
 export class AppModule {}
