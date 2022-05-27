b5ae7a949bb33cce727e342f939e29c5c9af2ebb
o.muslimov <oybekmuslimov15@gmail.com>
2022-05-27 20:18:14 +0300

    Revert "Revert "Поменял ссылки""
    This reverts commit 8deec0d2e080f1563e40ea19bce548f15d036c2e.

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -2,4 +2,5 @@ export const isProduction = process.env.NODE_ENV === 'production';
 
 export const appPort = process.env.APP_PORT || 5050;
 export const appHost = process.env.APP_HOST || 'localhost';
+export const backendUrl = process.env.BACKEND_URL;
 
